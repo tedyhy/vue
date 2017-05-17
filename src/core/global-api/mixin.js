b0e9e745d18ad013b6cfafbc436efafd5fe1b983
tedyhy <916610351@qq.com>
2017-05-17 16:15:17 +0800

    global-api/mixin

--- src/core/global-api/mixin.js
+++ src/core/global-api/mixin.js
@@ -2,6 +2,13 @@
 
 import { mergeOptions } from '../util/index'
 
+/**
+ * 全局注册一个混合，影响注册之后所有创建的每个 Vue 实例。
+ * 插件作者可以使用混合，向组件注入自定义的行为。
+ * 不推荐在应用代码中使用。
+ * 参考 https://vuejs.org/v2/guide/mixins.html
+ * @param Vue
+ */
 export function initMixin (Vue: GlobalAPI) {
   Vue.mixin = function (mixin: Object) {
     this.options = mergeOptions(this.options, mixin)
