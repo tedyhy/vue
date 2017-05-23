57662a5b7b63e447a6d1142d6dfdb7613e48ac60
tedyhy <916610351@qq.com>
2017-05-23 18:38:34 +0800

    src/core/observer/array

--- src/core/observer/array.js
+++ src/core/observer/array.js
@@ -5,11 +5,14 @@
 
 import { def } from '../util/index'
 
+// Array 原型
 const arrayProto = Array.prototype
+// 定义继承 Array.prototype 对象的对象
 export const arrayMethods = Object.create(arrayProto)
 
 /**
  * Intercept mutating methods and emit events
+ * 拦截方法并触发相关事件
  */
 ;[
   'push',
@@ -22,16 +25,21 @@ export const arrayMethods = Object.create(arrayProto)
 ]
 .forEach(function (method) {
   // cache original method
+  // 缓存原始方法
   const original = arrayProto[method]
   def(arrayMethods, method, function mutator () {
     // avoid leaking arguments:
     // http://jsperf.com/closure-with-arguments
+    // 避免内存泄漏
+    // 避免引用，重新定义数组用来存储参数
     let i = arguments.length
     const args = new Array(i)
     while (i--) {
       args[i] = arguments[i]
     }
+    // 执行原始方法，保存其结果
     const result = original.apply(this, args)
+    // 取对象 arrayMethods 属性 __ob__
     const ob = this.__ob__
     let inserted
     switch (method) {
